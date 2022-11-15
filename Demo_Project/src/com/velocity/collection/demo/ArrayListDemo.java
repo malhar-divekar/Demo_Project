4118b465da3f006575c31e27f19fe995a2b486cf
malhar-divekar <malhardivekar7@gmail.com>
2022-11-15 20:54:36 +0530

    First commit of Demo_Project

---
+++ Demo_Project/src/com/velocity/collection/demo/ArrayListDemo.java
@@ -0,0 +1,26 @@
+package com.velocity.collection.demo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList arrayList = new ArrayList();
+		arrayList.add("Ram");
+		arrayList.add("Shyam");
+		arrayList.add("Gopal");
+		arrayList.add("Arjun");
+		
+		System.out.println("ArryList contains :"+arrayList);
+		
+		Iterator itr = arrayList.iterator();
+		
+		while (itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+			
+	}
+
+}
